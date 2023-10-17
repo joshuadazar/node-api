025747e2c73de1c8d4cca9f8080850544d8537a3
joshua daza rincon <joshuadazar@gmail.com>
2023-10-17 12:12:36 -0500

    Update index.js with random number request "/data"

--- index.js
+++ index.js
@@ -7,5 +7,9 @@ const port = 3100;
 const welcome = require('./routes/welcome')
 
 app.use("", welcome);
+app.use('/data', (req, res)=> {
+  let data = Math.floor(Math.random()*10)
+  res.send(String(data));
+)
 
 app.listen(port, ()=> console.log(`Server on http://localhost:${port}`))
