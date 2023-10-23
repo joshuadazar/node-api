cf5af5c741ef812fad743411dbbe2e6ff301dee6
Joshua Daza <jdaza@hugeinc.com>
2023-10-23 11:39:47 -0500

    Update environment value

--- database/connection.js
+++ database/connection.js
@@ -1,7 +1,7 @@
 const pg = require('pg');
 
 const pool = new pg.Pool({
-  connectionString: 'postgres://projects_0t43_user:BZjHLJ0b3OLsSDQ3bluUl0MBJUGvlO2k@dpg-ckngbtj6vnjc73anm3e0-a.oregon-postgres.render.com/projects_0t43',
+  connectionString: process.env.DATABASE_URL,
   ssl: true
 });
 
--- index.js
+++ index.js
@@ -11,11 +11,11 @@ const welcome = require('./routes/welcome')
 const rnd = require('./routes/rnd')
 const query = require('./database/query')
 
-//app.get("/rnd", rnd);
+app.get("/rnd", rnd);
 app.get("/data", welcome);
 app.get("/query/:id", query)
 
-app.use('/rnd', async (req, res) => {
+app.use('', async (req, res) => {
   try {
   const result = await pool.query('SELECT NOW()')
   res.send(result.rows[0])
